504deabe3f2b83b4a80c4ec0d7177eb694e42187
FahmidRahman <20frahman@gmail.com>
2024-11-14 00:01:41 +0000

    jump added

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,7 +11,10 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] Camera targetCamera;
     [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float jumpForce = 5f; 
+    [SerializeField] LayerMask groundLayer;
 
+    bool isGrounded;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
@@ -24,36 +27,43 @@ public class CharacterMovement : MonoBehaviour
     private void Update() {
         HandleMovement();
         HandleRotation();
+        HandleJump();
     }
 
     private void HandleMovement() {
+        
         moveDirection = targetCamera.transform.forward * moveVectorInput.z;
         moveDirection += targetCamera.transform.right * moveVectorInput.x;
         moveDirection.y = 0f;
         moveDirection.Normalize();
 
-
+        
         Vector3 moveVelocity = moveDirection * speed;
-        moveVelocity += Physics.gravity;
+        moveVelocity.y = rb.velocity.y; 
 
         rb.velocity = moveVelocity;
-
     }
 
     private void HandleRotation() {
-        if(moveDirection.magnitude > 0f) {
+        if (moveDirection.magnitude > 0f) {
             rotationDirection = moveDirection;
         }
 
-        if(rotationDirection != Vector3.zero) {
+        if (rotationDirection != Vector3.zero) {
             Quaternion targetRotation = Quaternion.LookRotation(rotationDirection);
-            Quaternion rotation = Quaternion.Slerp(targetRotation, targetRotation, rotationSpeed * Time.deltaTime);
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
             transform.rotation = rotation;
         }
     }
 
+    private void HandleJump() {
+        
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.5f, groundLayer);
 
-    
+        
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
 }
-
